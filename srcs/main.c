2977e7e4fd06f53c102c642819f977f1b68361d0
anedonia <deuxvys@gmail.com>
2022-10-11 16:09:03 +0200

    main de robin

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ldevy <ldevy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 19:03:24 by rle-thie          #+#    #+#             */
-/*   Updated: 2022/10/11 16:02:23 by ldevy            ###   ########.fr       */
+/*   Updated: 2022/10/11 16:07:11 by ldevy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,23 +34,44 @@ t_data g_data;
 // 	return (0);
 // }
 
-int	main(int ac, char **av, char **envp)
+int main(int ac, char const **av, char **envp)
 {
-	(void)ac;
-	(void)av;
-	data = init_all(envp);
+    t_data *data;
+    char *str;
+    
+    (void)ac;
+    (void)av;
+    data = init_all(envp);
 
-	envp = envp;
-	while (1)
-	{
-		str = readline("Minishell : ");
-		lexer(str, data);
-		break;
-	}
-	str=str;
-	ft_garb_free_all(data);
-	return (0);
+    envp = envp;
+    while (1)
+    {
+        str = readline("Minishell : ");
+        lexer(str, data);
+        break;
+    }
+    str=str;
+    ft_garb_free_all(data);
+    return (0);
 }
+
+// int	main(int ac, char **av, char **envp)
+// {
+// 	(void)ac;
+// 	(void)av;
+// 	data = init_all(envp);
+
+// 	envp = envp;
+// 	while (1)
+// 	{
+// 		str = readline("Minishell : ");
+// 		lexer(str, data);
+// 		break;
+// 	}
+// 	str=str;
+// 	ft_garb_free_all(data);
+// 	return (0);
+// }
 
 // int main(int ac, char const **av, char **envp)
 // {
