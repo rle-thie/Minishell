c038c3dfd8211500bdcda31f8e674b9fa1a47e14
Robin <robinlethiec1@gmail.com>
2022-11-09 01:38:27 +0100

    FEATURE -> QUOTE - quote/dquote op. manque l'expand des variable d'env dans les dquotes. - manque la vrai fonction ou la methode correcte pour exite en cas de quote non ferme.

--- srcs/lexer/lexer_double_quote.c
+++ srcs/lexer/lexer_double_quote.c
@@ -6,36 +6,11 @@
 /*   By: rle-thie <rle-thie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 13:06:14 by rle-thie          #+#    #+#             */
-/*   Updated: 2022/11/08 23:41:23 by rle-thie         ###   ########.fr       */
+/*   Updated: 2022/11/09 00:47:25 by rle-thie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-t_token	*delete_lst_token(t_token *lst)
-{
-	if (!lst->prev && !lst->next)
-	{
-		g_data.cmd = NULL;
-		return (NULL);
-	}
-	else if (!lst->prev)
-	{
-		*lst = *(lst)->next;
-		lst->prev = NULL;
-	}
-	else if (!lst->next)
-	{
-		lst = lst->prev;
-		lst->next = NULL;
-	}
-	else if (lst->prev && lst->next)
-	{
-		lst->prev->next = lst->next;
-		lst->next->prev = lst->prev;
-	}
-	return (lst);
-}
 
 char	*create_one_char(char c)
 {
@@ -88,55 +63,73 @@ t_token	*add_before_token(t_token *lst, t_token_type type)
 }
 
 
-t_token	*check_double_quote(t_token *cmd)
+t_token	*check_double_quote(t_token *cmd, int i, int len)
 {
-	int len;
-	int	i;
-
-	i = 0;
-	len = 0;
 	while (cmd && cmd->next)
 	{
-		if (cmd->type == QUOTE && ft_strlen(cmd->str) > 1 && ft_strlen(cmd->str) % 2 == 0)
+		if (cmd->type == QUOTE && ft_strlen(cmd->str) > 1)
 		{
 			len = ft_strlen(cmd->str);
-			// cmd->str = create_one_char('\'');
-			while(i < len)
-			{
+			while(++i < len)
 				cmd = add_before_token(cmd, QUOTE);
-				i++;
-			}
 			cmd = delete_lst(cmd);
 		}
 		if (cmd && cmd->next)
 			cmd = cmd->next;
 	}
-	i = 0;
+	i = -1;
 	if (cmd)
 	{
-		if (cmd->type == QUOTE && ft_strlen(cmd->str) > 1 && ft_strlen(cmd->str) % 2 == 0)
+		if (cmd->type == QUOTE && ft_strlen(cmd->str) > 1)
 		{
 			len = ft_strlen(cmd->str);
-			cmd->str = create_one_char('\'');
-			while(i < len)
-			{
+			while(++i < len)
 				cmd = add_before_token(cmd, QUOTE);
-				i++;
-			}
+			cmd = delete_lst(cmd);
+		}
+		// if (cmd && cmd->next)
+		// 	cmd = cmd->next;
+	}
+	return (cmd);
+}
+
+t_token	*check_double_dquote(t_token *cmd, int i, int len)
+{
+	while (cmd && cmd->next)
+	{
+		if (cmd->type == DQUOTE && ft_strlen(cmd->str) > 1)
+		{
+			len = ft_strlen(cmd->str);
+			while(++i < len)
+				cmd = add_before_token(cmd, DQUOTE);
+			cmd = delete_lst(cmd);
 		}
 		if (cmd && cmd->next)
 			cmd = cmd->next;
+	}
+	i = -1;
+	if (cmd)
+	{
+		if (cmd->type == DQUOTE && ft_strlen(cmd->str) > 1)
+		{
+			len = ft_strlen(cmd->str);
+			while(++i < len)
+				cmd = add_before_token(cmd, DQUOTE);
+			cmd = delete_lst(cmd);
+		}
+		// if (cmd && cmd->next)
+		// 	cmd = cmd->next;
 	}
 	return (cmd);
 }
 
 t_token	*check_double_allquote(t_token *cmd)
 {
-	cmd = check_double_quote(cmd);
+	cmd = check_double_quote(cmd, -1, 0);
 	while (cmd && cmd->prev)
 		cmd = cmd->prev;
-	// cmd = check_double_dquote(cmd);
-	// while (cmd && cmd->prev)
-	// 	cmd = cmd->prev;
+	cmd = check_double_dquote(cmd, -1, 0);
+	while (cmd && cmd->prev)
+		cmd = cmd->prev;
 	return (cmd);
 }
--- srcs/lexer/lexer_quote.c
+++ srcs/lexer/lexer_quote.c
@@ -6,28 +6,53 @@
 /*   By: rle-thie <rle-thie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 18:28:34 by rle-thie          #+#    #+#             */
-/*   Updated: 2022/11/09 00:15:33 by rle-thie         ###   ########.fr       */
+/*   Updated: 2022/11/09 01:35:20 by rle-thie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+t_token	*delete_lst_tok(t_token *lst)
+{
+	if (!lst->prev && !lst->next)
+		return (NULL);
+	else if (!lst->prev)
+	{
+		lst = lst->next;
+		lst->prev = NULL;
+	}
+	else if (!lst->next)
+	{
+		lst = lst->prev;
+		lst->next = NULL;
+	}
+	else if (lst->prev && lst->next)
+	{
+		lst->prev->next = lst->next;
+		lst->next->prev = lst->prev;
+		lst = lst->next;
+	}
+	return (lst);
+}
 
 t_token	*delete_all_quote(t_token *cmd)
 {
 	while (cmd && cmd->next)
 	{
 		if (cmd->type == QUOTE || cmd->type == DQUOTE)
-			cmd = delete_lst(cmd);
-		else if (cmd && cmd->next)
+		{
+			cmd = delete_lst_tok(cmd);
+		}
+		else if (cmd->next)
 			cmd = cmd->next;
 	}
 	if (cmd)
 	{
 		if (cmd->type == QUOTE || cmd->type == DQUOTE)
-			cmd = delete_lst(cmd);
+		{
+			cmd = delete_lst_to(cmd);
+		}
 	}
-	if (cmd && cmd->prev)
-	cmd = cmd->prev;
 	// ft_print_token(cmd);
 	return (cmd);
 }
@@ -62,29 +87,17 @@ int	check_open_quote(t_token *cmd)
 // check_quote(data->cmd);
 t_token	*check_quote(t_token *cmd)
 {
-	// cmd = check_double_allquote(cmd);
+	cmd = check_double_allquote(cmd);
 	cmd = putin_allquote(cmd);
-	cmd = check_multiple_allquote(cmd);
+	// cmd = check_multiple_allquote(cmd);
 	if (cmd && !check_open_quote(cmd))
 		ft_exit_provisoire("quote ouvert\n", 2);
 	cmd = replace_allquote(cmd);
 	cmd = delete_all_quote(cmd);
-	ft_print_token(cmd);
-	// while (cmd && cmd->next)
-	// {
-	// 	if (cmd->type == QUOTE)
-	// 		cmd = lex_quote(cmd);
-	// 	if (cmd->next)
-	// 		cmd = cmd->next;
-	// }
-	// if (cmd)
-	// {
-	// 	if (cmd->type == QUOTE)
-	// 		cmd = lex_quote(cmd);
-	// }
+
 	while (cmd && cmd->prev)
 		cmd = cmd->prev;
-	ft_print_token(cmd);
+	// cmd = delete_all_quote(cmd);
 	// // ft_printstr("cxacacacacan\n");
 	// join_quote(cmd, NULL);
 	return (cmd);
