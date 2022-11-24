124e3e7977a4d11308ab693db1c7d765c11237f9
rle-thie <robinlethiec1@gmail.com>
2022-11-24 12:27:33 +0100

    NORME

--- include/init.h
+++ include/init.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldevy <ldevy@student.42.fr>                +#+  +:+       +#+        */
+/*   By: rle-thie <rle-thie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 11:16:50 by rle-thie          #+#    #+#             */
-/*   Updated: 2022/11/21 18:10:20 by ldevy            ###   ########.fr       */
+/*   Updated: 2022/11/24 12:27:23 by rle-thie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,36 @@
 # define INIT_H
 
 // prompt
-char	*input_name(void);
+char		*input_name(void);
 
 // garbage collector init/create
-void	ft_garb_add(t_data *data, t_garbage *new);
+void		ft_garb_add(t_data *data, t_garbage *new);
 t_garbage	*ft_garb_new(void *ptr_content, t_data *data);
 
 // ft_Malloc
-void	*ft_malloc(size_t size, t_data *data);
-void	*ft_calloc(size_t size, t_data *data);
+void		*ft_malloc(size_t size, t_data *data);
+void		*ft_calloc(size_t size, t_data *data);
 
 // free all & free addr
-void 	ft_garb_free_all(t_data *data);
-void	ft_free(void *ptr, t_data *data);
+void		ft_garb_free_all(t_data *data);
+void		ft_free(void *ptr, t_data *data);
 
 // init all
-void	init_all(char **envp);
+void		init_all(char **envp);
 void		init_garb(t_data *data);
 
 // init_env
-void	init_env(void);
-int		make_node(char *str);
-int		add_back_env(t_env *new);
-int		find_signe(char *str);
-int		env_size(void);
-void	spe_env(void);
+void		init_env(void);
+int			make_node(char *str);
+int			add_back_env(t_env *new);
+int			find_signe(char *str);
+int			env_size(void);
+void		spe_env(void);
 
 // fd_opened;
 t_opened	*ft_opened_new(int fd, t_data *data);
-void	opened_addback(t_opened *new, t_data *data);
-void	close_all(t_data *data);
-int	ft_open(char *filename, int flags, int mod, t_data *data);
+void		opened_addback(t_opened *new, t_data *data);
+void		close_all(t_data *data);
+int			ft_open(char *filename, int flags, int mod, t_data *data);
 
 #endif
--- include/parsing.h
+++ include/parsing.h
@@ -6,92 +6,92 @@
 /*   By: rle-thie <rle-thie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 11:17:07 by rle-thie          #+#    #+#             */
-/*   Updated: 2022/11/24 11:29:43 by rle-thie         ###   ########.fr       */
+/*   Updated: 2022/11/24 12:26:38 by rle-thie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	PARSING_H
+#ifndef PARSING_H
 # define PARSING_H
 
 // lexer
-void	lexer(char *str, t_data *data);
-void	create_cmd(t_token *cmd, t_token *token, t_data *data, int len);
+void			lexer(char *str, t_data *data);
+void			create_cmd(t_token *cmd, t_token *token, t_data *data, int len);
 
 // lexer_cmd_utils.c
-char	*trans(char c, t_data *data);
+char			*trans(char c, t_data *data);
 t_token_type	select_type(char c);
-void	fill_type(t_token *cmd, t_data *data);
+void			fill_type(t_token *cmd, t_data *data);
 
 // lexer_space.c
-void	lexer_space(t_token *cmd);
+void			lexer_space(t_token *cmd);
 
 // variable d'env
-t_token	*check_variable_env(t_token *cmd);
-t_token	*check_double_dollar(t_token *cmd, int i, int len);
-t_token	*expand_var(t_token *cmd);
-char	*expand_status(char *str);
-char	*expand_varchar(char *str, int i);
+t_token			*check_variable_env(t_token *cmd);
+t_token			*check_double_dollar(t_token *cmd, int i, int len);
+t_token			*expand_var(t_token *cmd);
+char			*expand_status(char *str);
+char			*expand_varchar(char *str, int i);
 
 // quote
-t_token	*check_quote(t_token *cmd);
-t_token	*check_multiple_allquote(t_token *cmd);
-t_token	*putin_allquote(t_token *cmd);
-t_token	*replace_allquote(t_token *cmd);
-t_token	*check_double_allquote(t_token *cmd);
-
+t_token			*check_quote(t_token *cmd);
+t_token			*check_multiple_allquote(t_token *cmd);
+t_token			*putin_allquote(t_token *cmd);
+t_token			*replace_allquote(t_token *cmd);
+t_token			*check_double_allquote(t_token *cmd);
 
 // flags
-char	*fill_flags(t_token *cmd);
+char			*fill_flags(t_token *cmd);
 // flags_utils.c
-char	*put_tiret(char *str);
+char			*put_tiret(char *str);
 
 // args
-char	**fill_args(t_token *cmd);
+char			**fill_args(t_token *cmd);
 
 // flags & args
-char	**fill_flags_args(char *flags, char **args, int nbr_args, char *cmd);
+char			**fill_flags_args(char *flags, char **args,
+					int nbr_args, char *cmd);
 
 // booleen
-void	add_bool_var(t_cmd	*format_cmd);
+void			add_bool_var(t_cmd	*format_cmd);
 
 // redir.c
-t_redir	*parse_redir(t_token *cmd, int exit_s);
+t_redir			*parse_redir(t_token *cmd, int exit_s);
 // redir_parse_lst.c
-t_redir	*format_redir_lst(t_redir *lst);
-int		ft_strleni(char *str, int i);
-int		check_one_redir(char *str);
+t_redir			*format_redir_lst(t_redir *lst);
+int				ft_strleni(char *str, int i);
+int				check_one_redir(char *str);
 // parser_redir_clean.c
-t_redir	*clean_all_redir(t_redir *lst);
-t_redir	*check_heredoc(t_redir *lst, char *content);
-char	*expand_heredoc(char *str, int i);
+t_redir			*clean_all_redir(t_redir *lst);
+t_redir			*check_heredoc(t_redir *lst, char *content);
+char			*expand_heredoc(char *str, int i);
 // redir_utils.c
-t_redir	*ft_create_redir(char *str);
-t_redir	*add_back_redir(t_redir *token, char *str);
-t_redir	*lst_put_start(t_redir *lst);
-t_token	*lst_start_token(t_token *lst);
-int		is_redir(t_token *cmd);
+t_redir			*ft_create_redir(char *str);
+t_redir			*add_back_redir(t_redir *token, char *str);
+t_redir			*lst_put_start(t_redir *lst);
+t_token			*lst_start_token(t_token *lst);
+int				is_redir(t_token *cmd);
 // heredoc
-int		heredoc_loop_return(char *content, char *line, char *eof);
-int		heredoc_loop(char **content, char *eof);
-int		create_heredoc(char *str, char *content);
-t_redir	*check_heredoc(t_redir *lst, char *content);
-int		ft_strcmp(char *s1, char *s2);
+int				heredoc_loop_return(char *content, char *line, char *eof);
+int				heredoc_loop(char **content, char *eof);
+int				create_heredoc(char *str, char *content);
+t_redir			*check_heredoc(t_redir *lst, char *content);
+int				ft_strcmp(char *s1, char *s2);
 // heredoc utils
-void	sig_reset(void);
-void	free_all(t_data *data);
-int		exit_heredoc_fork(t_data *data, int dup_stdin);
-void	exec_error(t_data *data);
-int		no_line_return(int i, char *eof);
+void			sig_reset(void);
+void			free_all(t_data *data);
+int				exit_heredoc_fork(t_data *data, int dup_stdin);
+void			exec_error(t_data *data);
+int				no_line_return(int i, char *eof);
 // heredoc utils utils
-char	*del_last_n(char *str);
-char	*put_heredoc(char *file);
-void	copy_in_heredoc(int fd, char *s);
-int		check_eof(char *line, char *eof);
+char			*del_last_n(char *str);
+char			*put_heredoc(char *file);
+void			copy_in_heredoc(int fd, char *s);
+int				check_eof(char *line, char *eof);
 // delete_chevron
-t_redir	*delete_chevron(t_redir *lst);
-int		check_redir_error(t_redir *lst);
-int		is_valid_redir(t_redir *lst);
+t_redir			*delete_chevron(t_redir *lst);
+int				check_redir_error(t_redir *lst);
+int				is_valid_redir(t_redir *lst);
 // parsing
-void	parser(void);
+void			parser(void);
 
 #endif
